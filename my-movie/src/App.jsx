787549104a0030f03a1f8513473179e2278ecd21
Christopher <tchry902@gmail.com>
2024-01-09 20:30:49 +0100

    creazione rotte e rotta priv., comp.logica privat root

---
+++ my-movie/src/App.jsx
@@ -0,0 +1,53 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PrivateRoute from "./Utility/privateRoute";
+
+import HomePage from "./Page/HomePage/index.tsx";
+import Film from "./Page/Film/index.jsx";
+import Preferiti from "./Page/Preferiti/index.jsx";
+
+import LoginPage from "./Page/LogInPage/index.jsx";
+import useStore from "./Utility/useStore/store.jsx";
+import NotFoundPage from "./Page/pageNotFound/index.jsx";
+
+function App() {
+  const isAuthenticated = useStore((state) => state.isAuthenticated);
+  //const [count, setCount] = useState(0);
+
+  console.log("*********************", isAuthenticated);
+
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <PrivateRoute
+              element={HomePage}
+              isAuthenticated={isAuthenticated}
+            />
+          }
+        />
+        <Route
+          path="/film"
+          element={
+            <PrivateRoute element={Film} isAuthenticated={isAuthenticated} />
+          }
+        />
+        <Route
+          path="/preferiri"
+          element={
+            <PrivateRoute
+              element={Preferiti}
+              isAuthenticated={isAuthenticated}
+            />
+          }
+        />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ my-movie/src/Utility/privateRoute.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react/prop-types */
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+
+const PrivateRoute = ({
+  isAuthenticated,
+
+  element: Element,
+  ...rest
+}) => {
+  const localValue = localStorage.getItem("logKey");
+  const newLocaValueObj = JSON.parse(localValue);
+
+  const isLog =
+    newLocaValueObj.success || isAuthenticated == true ? true : false;
+  console.log(isAuthenticated, "😅😅😅😅😅😅😅😅😅😅😅😅😅😅😅😅");
+
+  return isLog ? <Element {...rest} /> : <Navigate to="/login" />;
+};
+
+PrivateRoute.propTypes = {
+  element: PropTypes.elementType.isRequired,
+};
+
+export default PrivateRoute;
